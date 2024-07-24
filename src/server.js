b376188989a4907ac4415a587fac8c11e0ae1ce5
beom-yun <scds0326@naver.com>
2024-07-24 10:09:07 +0900

    WebSocket Messages

--- src/server.js
+++ src/server.js
@@ -15,9 +15,11 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-function handleConnection(socket) {
-  console.log(socket);
-}
-wss.on("connection", handleConnection);
+wss.on("connection", (socket) => {
+  console.log("Connected to the browser ✅");
+  socket.on("close", () => console.log("Disconnected from the browser ❌"));
+  socket.on("message", (message) => console.log(message.toString("utf-8")));
+  socket.send("hello!");
+});
 
 server.listen(3000, handleListen);
